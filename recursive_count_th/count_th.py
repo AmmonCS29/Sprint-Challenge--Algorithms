565168e420e9e871dccf3da5ab70fcb6c562b2bd
AmmonMatthews <ammonmatthews@gmail.com>
2020-05-21 23:09:21 -0600

    MVP and Stretch

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,18 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+'''
+returning the length of a word set character les than two or zero if th does not exist
+define what I am looking for in th in lower case
+I only want to have two characters
+returning the instance of the th string
+if its just a individual character return nothing
+I can multiple the th together in one word by just adding it to the count
+'''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    if len(word) <2:
+        return 0
+    if word[:2] == 'th':
+        return 1 + count_th(word[2:])
+    else:
+        return 0 + count_th(word[1:])
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,3 +1,19 @@
+'''
+Understanding:
+Use the bubble sorting method
+
+The objective of the sorting robot is to take a list and sort the integers from lowest to highest. 
+
+ The robot is given alist and then it can move left or right to evaluate the value. 
+ The initial position is at index 0, initial item value is none. 
+ 
+Planning:
+Need to turn the Light On to start and turn the light off to stop
+
+
+
+'''
+
 class SortingRobot:
     def __init__(self, l):
         """
@@ -91,13 +107,96 @@ class SortingRobot:
         Returns True if the robot's light is on and False otherwise.
         """
         return self._light == "ON"
+    
+
 
     def sort(self):
         """
         Sort the robot's list.
+        if self.light_is_on is false:
+            self.set_light_on()
+        
+        Move to the end of the list and swap the last value with none
+        Then return to the begining and compare the first value with the current item. 
+        
+        if that is 1 then swap it and move back to the beginning. if it is 0 or -1 keep it and move right one then call the function again.
+
+
         """
+
+        # while True:
+        #     self.set_light_off()
+        #     while self.can_move_right():
+        #         self.swap_item()
+        #         self.move_right()
+        #         if self.compare_item() == 1:
+        #             self.swap_item()
+        #             self.set_light_on()
+        #         self.move_left()
+        #         self.swap_item()
+        #         self.move_right()
+        #     if not self.light_is_on():
+        #         break
+        #     while self.can_move_left():
+        #         self.move_left()
+        
+        
+        self.set_light_on() # Turning the light on
+        self.swap_item() # swapping none for initial value
+        while self.light_is_on(): # while the light is on do the following
+            self.set_light_off() # Turn the light off
+            while self.can_move_right(): # While the robot is capable of moving right
+                self.move_right() # move right one space
+                if self.compare_item() == 1: #compare item if the returned value is 1 move to line 103 else move to line 104
+                    self.set_light_on() # turn light on and move to line 106
+                elif self.compare_item() == -1: # if value returned is -1 move to line 105 otherwise move to line 106
+                    self.swap_item() # swap the item at the current position
+            while self.can_move_left(): # while the robot can move left
+                if self.compare_item() == 1: # compare the item at the current position. if the value returned is 1 move to line 108 else move to line 109
+                    self.swap_item() # swap the item at the current position
+                elif self.compare_item() == -1: # compare the item at the current postion. if the value returned is -1 move to line 110 else move to line 11
+                    self.set_light_on() # set the light to on
+                self.move_left() # move left one space and repeat loop on line 98
+        self.swap_item() # swap the item at current position when the light is turned of and loop on 98 is finished
+
+        # self.set_light_on()
+        # # self.swap_item()
+        # while self.can_move_right() is True:
+        #     self.move_right()
+        
+        # if self.can_move_right() is False:
+        #     self.swap_item()
+
+        # while self.can_move_left() is True:
+        #     self.move_left()
+
+        # self.sort_again()
+        # print("item", self._item)
+        # print("position", self._position)
+        # self.set_light_off()  
+
+
+
+
         # Fill this out
-        pass
+        # if self.light_is_on() is False:
+        #     self.set_light_on()
+        
+        # while self.can_move_right() is True:
+        #     self.move_right()
+        
+        # if self.can_move_right() is False:
+        #     self.swap_item()
+
+        # while self.can_move_left() is True:
+        #     self.move_left()
+
+        # print("item", self._item)
+        # print("position", self._position)
+        
+
+
+
 
 
 if __name__ == "__main__":
@@ -105,6 +204,8 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+
+    # l= [1,2,3,4,5,6]
 
     robot = SortingRobot(l)
 
